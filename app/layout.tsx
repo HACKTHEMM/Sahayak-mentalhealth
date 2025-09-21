f60a0d26cbf8c5b894444b5a6c12faf0d4d7a72d
Vinitj088 <vinitjain088@gmail.com>
2025-09-21 14:38:17 +0530

    updated branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Sahayak AI',
+  description: 'Your Mental Health Buddy'
 }
 
 export default function RootLayout({
